9018de2bdd109fdff28881b61711a79ada592763
Hamza Shadeez <hahmad1178@gmail.com>
2024-11-11 14:59:28 +0100

    update

--- Hamza Movies/js/application.js
+++ Hamza Movies/js/application.js
@@ -3,8 +3,7 @@ const form = document.getElementById("search-form");
 const input = document.getElementById("input");
 
 const searchMusic = async (search) => {
-    const url =
-    `https://shazam.p.rapidapi.com/search?term=${search}&locale=en-US&offset=0&limit=9`;
+  const url = `https://shazam.p.rapidapi.com/search?term=${search}&locale=en-US&offset=0&limit=9`;
   const options = {
     method: "GET",
     headers: {
@@ -22,26 +21,53 @@ const searchMusic = async (search) => {
     showMusicOnPage(songs);
   } catch (error) {
     console.error(error);
-  } 
-
+  }
 };
 
 const showMusicOnPage = (hits) => {
   // hits variable is an array data type
   const container = document.querySelector("#music-container");
-  container.innerHTML = ""
+  container.innerHTML = "";
 
   hits.map((data) => {
     const title = data.track.title;
     const image = data.track.images.background;
+    // create main div -- track
+    const div = document.createElement("div");
+    div.classList.add("track");
+    // create an image
+    const img = document.createElement("img");
+    img.src = image;
+    img.classList.add(..."w-full object-contain".split(" "));
 
-    let htmlCode = ` <div class="track">
-            <img src="${image}" alt="" class="w-full object-contain" >
-            <h1 class="text-green-600 text-clip-1 w-full truncate font-semibold text-xl mt-3">${title}</h1>
-            <button class="bg-green-600 px-12 mt-4 py-2 rounded-sm text-sm text-white font-semibold">Play</button>
-         </div>`;
+    // create a h1 element for the title
+    const h1 = document.createElement("h1");
+    h1.classList.add(
+      ..."text-green-600 text-clip-1 w-full truncate font-semibold text-xl mt-3".split(
+        " "
+      )
+    );
+    h1.textContent = title;
 
-    container.innerHTML += htmlCode;
+    // create button
+    const button = document.createElement("button");
+    button.classList.add(
+      ..."bg-green-600 px-12 mt-4 py-2 rounded-sm text-sm text-white font-semibold".split(
+        " "
+      )
+    );
+    button.innerText = "Play";
+    button.addEventListener("click", () => {
+      const audio = new Audio(data.track.hub?.actions[1].uri);
+      audio.play();
+    });
+
+    // append html elements
+    div.appendChild(img);
+    div.appendChild(h1);
+    div.appendChild(button);
+
+    container.appendChild(div);
   });
 };
 
@@ -54,7 +80,7 @@ form.addEventListener("submit", (e) => {
 
 const getFreeMusic = async () => {
   const url =
-    "https://shazam.p.rapidapi.com/search?term=love%20me&locale=en-US&offset=0&limit=9";
+    "https://shazam.p.rapidapi.com/search?term=yusuf%20islam&locale=en-US&offset=0&limit=9";
   const options = {
     method: "GET",
     headers: {
